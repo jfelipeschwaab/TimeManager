ca7075903b5669eb99ad29b954e49517cdd16a24
jfelipeschwaab <joaofsp.games@gmail.com>
2025-08-12 16:44:15 -0300

    Initial Commit

---
+++ TimeManager/TimeManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeManagerApp.swift
+//  TimeManager
+//
+//  Created by João Felipe Schwaab on 12/08/25.
+//
+
+import SwiftUI
+
+@main
+struct TimeManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
